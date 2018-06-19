88163feac9400aba6d8185369ff95aa8cea669f1
joselimaico <joselimaicotele@gmail.com>
2018-06-19 16:10:11 -0500

    dos servidores se conectan

---
+++ backend/api/models/Pulsacion.js
@@ -0,0 +1,22 @@
+/**
+ * Pulsacion.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombreRaspberry:{
+      type:'string',
+      required:true
+    },
+    usuarioIdFK:{
+      model:'Usuario'
+    }
+
+  },
+
+};
+
